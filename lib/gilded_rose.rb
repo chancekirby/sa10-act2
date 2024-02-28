5dfafd6345c702036487f7a303ee973f8f7f02b0
Chance Kirby <chance14@comcast.net>
2024-02-27 21:32:20 -0600

    Implemented sulfuras_tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,6 +13,8 @@ class GildedRose
       return normal_tick
     when 'Aged Brie'
       return brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
     end
 
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
@@ -72,5 +74,8 @@ class GildedRose
     return if @quality >= 50
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
+
+  def sulfuras_tick
   end
 end
