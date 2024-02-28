8022ffbe7f7ea2b2071328c758538a55aebdfed4
Chance Kirby <chance14@comcast.net>
2024-02-27 22:12:15 -0600

    Refactored to make a Brie class with method tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,10 +26,8 @@ class GildedRose
   end
 
   def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
+    @item = Brie.new(quality, days_remaining)
+    @item.tick
   end
 
   def sulfuras_tick
@@ -60,6 +58,21 @@ class GildedRose
     
   end
 
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+
+  end
   def quality
     return @item.quality if @item
     @quality
